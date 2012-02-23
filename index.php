8c0a2ed947208f67cdba176b4ab2a35616daa554
Matthew Reidsma <matthew@reidsrow.com>
2012-02-22 19:54:54 -0500

    First commit.

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+
+$patron_id = $_GET['p'];
+$item_id = $_GET['i'];
+
+?>
+
+<form action="https://library.catalog.gvsu.edu/patroninfo~S17/<?php echo $patron_id; ?>/items" name="checkout_form" method="POST" id="checkout_form" >
+<inut type="hidden" name="requestRenewSome" value="requestRenewSome" />	
+<input type="hidden" name="renew0" id="renew0" value="<?php echo $item_id; ?>" />
+</form>
+
+<script>
+document.checkout_form.submit();
+</script>
+<noscript><input type="submit" value="Confirm" /></noscript>

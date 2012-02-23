dcbfd359645c2f2fdd6b9f6d7fc463a515c7bf2b
Matthew Reidsma <matthew@reidsrow.com>
2012-02-22 21:14:16 -0500

    Fixed a typo in the input field.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $item_id = $_GET['i'];
 ?>
 
 <form action="https://library.catalog.gvsu.edu/patroninfo~S17/<?php echo $patron_id; ?>/items" name="checkout_form" method="POST" id="checkout_form" >
-<inut type="hidden" name="requestRenewSome" value="requestRenewSome" />	
+<input type="hidden" name="requestRenewSome" value="requestRenewSome" />	
 <input type="hidden" name="renew0" id="renew0" value="<?php echo $item_id; ?>" />
 </form>
 
